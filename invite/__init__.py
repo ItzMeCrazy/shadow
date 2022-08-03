b55e41b18b6deccce2595ffa65062e4d90b1e581
AmazingAkai <azharmomin420@gmail.com>
2022-08-03 11:36:41 +0530

    Updated __init__

--- invite/__init__.py
+++ invite/__init__.py
@@ -32,7 +32,7 @@ class Invite(commands.Cog):
         embed.description = f'Thanks for choosing to invite {ctx.bot.user.name} to your server.'
         embed.set_thumbnail(url=ctx.bot.user.avatar or ctx.bot.user.default_avatar)
         embed.set_author(name=ctx.bot.user.name, icon_url=ctx.bot.user.avatar)
-        embed.set_footer(name=ctx.guild.name or 'Thanks for inviting.', icon_url=ctx.guild.icon or ctx.bot.user.avatar or ctx.bot.user.default_avatar)
+        embed.set_footer(text=ctx.guild.name or 'Thanks for inviting.', icon_url=ctx.guild.icon or ctx.bot.user.avatar or ctx.bot.user.default_avatar)
         embed.add_field(name='Invite the bot.', value=f'[Click here to invite]({url})')
         await ctx.send(embed=embed)
 
