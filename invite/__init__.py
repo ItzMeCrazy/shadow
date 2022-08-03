84a724beb580a607d7685b1759cc5b55ec05b1b3
AmazingAkai <azharmomin420@gmail.com>
2022-08-03 11:30:41 +0530

    Updated __init__

--- invite/__init__.py
+++ invite/__init__.py
@@ -31,7 +31,7 @@ class Invite(commands.Cog):
         url = f'https://discord.com/api/oauth2/authorize?client_id={ctx.bot.user.id}&permissions=8&scope=bot+applications.commands'
         embed.description = f'Thanks for choosing to invite {ctx.bot.user.name} to your server.'
         embed.set_thumbnail(url=ctx.bot.user.avatar or ctx.bot.user.default_avatar)
-        embed.set_author(name=ctx.bot.user.name, icon_url=ctx.bot.avatar)
+        embed.set_author(name=ctx.bot.user.name, icon_url=ctx.bot.user.avatar)
         embed.set_footer(name=ctx.guild.name or 'Thanks for inviting.', icon_url=ctx.guild.icon or ctx.bot.user.avatar or ctx.bot.user.default_avatar)
         embed.add_field(name='Invite the bot.', value=f'[Click here to invite]({url})')
         await ctx.send(embed=embed)
