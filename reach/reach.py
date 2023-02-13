4378f7b94b0ede65d1d7816146a84dd44c2d9ab0
AmazingAkai <azharmomin420@gmail.com>
2023-02-13 21:13:53 +0530

    Updated reach command.

--- reach/reach.py
+++ reach/reach.py
@@ -20,17 +20,18 @@ class Reach(commands.Cog):
     ):
         """Shows the reach of roles in a channel"""
         members = 0
-        total_members = len(channel.guild.members)
+        total_members = 0
         for role in roles:
-            dummy_member = role.members[0]
-            if channel.permissions_for(dummy_member).read_messages:
-                members += len(role.members)
+            total_members += len(role.members)
+            for member in role.members:
+                if channel.permissions_for(member).read_messages:
+                    members += 1
 
         percent = 100 * members / total_members
         description = (
             f"Channel: {channel.mention} `{channel.id}`\n\n"
             + "\n".join(
-                f"<a:arrow:1050149168586440765> {role.mention} `{role.id}` members: {len(role.members)} reach: {100 * len(role.members) / total_members:.2f}%"
+                f"<:Arrow:1074035208640286810> {role.mention} `{role.id}` members: {len(role.members)} reach: {100 * len(role.members) / total_members:.2f}%"
                 for role in roles
             )
             + f"\nTotal reach: {members} out of {total_members} targeted members\nwhich represents {percent:.2f}%"
