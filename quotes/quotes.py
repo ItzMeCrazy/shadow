9acd007b00e9dd95bbc2ee68dbe29392ecac876d
AmazingAkai <azharmomin420@gmail.com>
2023-02-13 20:15:21 +0530

    Fixed qotd.

--- quotes/quotes.py
+++ quotes/quotes.py
@@ -14,7 +14,7 @@ class Quotes(commands.Cog):
 
     @commands.command(aliases=["qotd"])
     async def quoteoftheday(self, ctx: commands.Context) -> None:
-        """Displays the device user is using."""
+        """Shows quote of the day."""
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 "https://quotes.rest/qod?language=en",
@@ -28,5 +28,24 @@ class Quotes(commands.Cog):
                     embed.set_author(
                         name=ctx.author.name, icon_url=ctx.author.avatar_url
                     )
-                    embed.set_footer(name=ctx.guild.name, value=ctx.guild.icon_url)
+                    embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url)
+                    await ctx.send(embed=embed)
+
+    @commands.command()
+    async def quote(self, ctx: commands.Context) -> None:
+        """Shows a random quote"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://quotes.rest/quote/random?language=en&limit=1",
+                headers={"accept": "application/json"},
+            ) as resp:
+                if resp.status == 200:
+                    json_response = await resp.json()
+                    quote = json_response["contents"]["quotes"][0]["quote"]
+                    embed = discord.Embed(title="Random Quote!", color=0x2F3136)
+                    embed.description = quote
+                    embed.set_author(
+                        name=ctx.author.name, icon_url=ctx.author.avatar_url
+                    )
+                    embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url)
                     await ctx.send(embed=embed)
